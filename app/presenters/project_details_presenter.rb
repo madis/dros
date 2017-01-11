f142ae30eab81533f1288e52753849f4ca5fe702
Madis Nõmme <madis.nomme@gmail.com>
2017-01-11 12:33:07 +0200

    Add health chart view with example data

--- app/presenters/project_details_presenter.rb
+++ app/presenters/project_details_presenter.rb
@@ -15,6 +15,22 @@ class ProjectDetailsPresenter < ProjectPresenter
     ]
   end
 
+  def health_assessments
+    labels = Date::MONTHNAMES.compact.map { |n| n[0..2] }
+    {
+      labels: labels,
+      series: [(1..labels.size).map { rand(100) }]
+    }
+  end
+
+  def health_change
+    44
+  end
+
+  def last_updated
+    @project.updated_at
+  end
+
   def random_value
     rand(0..1000)
   end
